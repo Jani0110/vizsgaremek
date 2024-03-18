0d70e37ae8b234f50c3c18e572ab6f650e8e86fa
Jani0110 <112945267+Jani0110@users.noreply.github.com>
2024-03-18 22:48:40 +0100

    Add files via upload
    Backend jelenleg

---
+++ chineseBackend/Modells/Etel.cs
@@ -0,0 +1,23 @@
+﻿namespace chineseBackend.Modells
+{
+   using System.ComponentModel.DataAnnotations;
+
+    public class Etel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? Nev { get; set; }
+
+        public string? Leiras { get; set; }
+
+        [Required]
+        public decimal Ar { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string? Kategoria { get; set; }
+    }
+}
